00ecbd2024b44b96eb5413dd72fff3ae5949c27e
Erik Kückelheim <kueckelheim.erik@gmail.com>
2025-04-29 13:38:38 +0200

    feat: hero section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,80 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="bg-white">
+	<header class="absolute inset-x-0 top-0 z-50">
+		<nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+			<div class="flex lg:flex-1">
+				<a href="#" class="-m-1.5 p-1.5">
+					<code class="font-semibold tracking-tighter">Erik Nogueira Kückelheim</code>
+				</a>
+			</div>
+			<div class="flex lg:hidden">
+				<button
+					type="button"
+					class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+				>
+					<span class="sr-only">Open main menu</span>
+					<svg
+						class="size-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+						data-slot="icon"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+						/>
+					</svg>
+				</button>
+			</div>
+			<div class="hidden lg:flex lg:gap-x-12">
+				<a href="#skills" class="text-sm/6 font-semibold text-gray-900">Skills</a>
+				<a href="#experience" class="text-sm/6 font-semibold text-gray-900">Experience</a>
+				<a href="#contact" class="text-sm/6 font-semibold text-gray-900">Contact</a>
+			</div>
+		</nav>
+		<!-- Mobile menu, show/hide based on menu open state. -->
+		<div class="lg:hidden" role="dialog" aria-modal="true">
+			<!-- Background backdrop, show/hide based on slide-over state. -->
+			<div class="fixed inset-0 z-50"></div>
+			<div
+				class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+			>
+				<div class="flex items-center justify-between">
+					<a href="#" class="-m-1.5 p-1.5">
+						<code class="font-semibold tracking-tighter">Erik Nogueira Kückelheim</code>
+					</a>
+					<button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+						<span class="sr-only">Close menu</span>
+						<svg
+							class="size-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true"
+							data-slot="icon"
+						>
+							<path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+						</svg>
+					</button>
+				</div>
+				<div class="mt-6 flow-root">
+					<div class="-my-6 divide-y divide-gray-500/10">
+						<div class="space-y-2 py-6 flex flex-col">
+							<a href="#skills" class="text-sm/6 font-semibold text-gray-900">Skills</a>
+							<a href="#experience" class="text-sm/6 font-semibold text-gray-900">Experience</a>
+							<a href="#contact" class="text-sm/6 font-semibold text-gray-900">Contact</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="relative isolate px-6 pt-14 lg:px-8">
+	<svg
+		class="absolute inset-0 -z-10 size-full [mask-image:radial-gradient(150%_150%_at_top_right,white,transparent)] stroke-gray-200"
+		aria-hidden="true"
+	>
+		<defs>
+			<pattern
+				id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+				width="200"
+				height="200"
+				x="50%"
+				y="-1"
+				patternUnits="userSpaceOnUse"
+			>
+				<path d="M.5 200V.5H200" fill="none" />
+			</pattern>
+		</defs>
+		<rect
+			width="100%"
+			height="100%"
+			stroke-width="0"
+			fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)"
+		/>
+	</svg>
+	<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-48">
+		<div class="text-center">
+			<img
+				src="/erik_kueckelheim.png"
+				alt="Erik Kückelheim"
+				class="mx-auto mb-8 h-52 w-52 ring-2 ring-gray-100 rounded-full object-cover"
+			/>
+			<h1 class="tracking- text-5xl font-semibold text-balance text-gray-900 sm:text-7xl">
+				Erik Nogueira Kückelheim
+			</h1>
+			<p class="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+				Creator, Freelancer, and Software Engineer
+			</p>
+			<div class="mt-10 flex items-center justify-center gap-x-6">
+				<a
+					href="#"
+					class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>Get started</a
+				>
+				<a href="#" class="text-sm/6 font-semibold text-gray-900"
+					>Learn more <span aria-hidden="true">→</span></a
+				>
+			</div>
+		</div>
+	</div>
+</div>
