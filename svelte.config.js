b928df1e6d844272119374e920e0639436150e31
Erik Kückelheim <kueckelheim.erik@gmail.com>
2025-04-29 13:55:40 +0200

    chore: prerender

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		prerender: {
+			crawl: true,
+			entries: ['*']
+		}
+	}
 };
 
 export default config;
